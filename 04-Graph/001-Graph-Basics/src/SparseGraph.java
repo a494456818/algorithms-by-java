e39c30de03bd22cfadbec01bb6483bd5b8feb8d5
a494456818 <494456818@qq.com>
2019-03-26 14:40:21 +0800

    Update SparseGraph.java

--- 04-Graph/001-Graph-Basics/src/SparseGraph.java
+++ 04-Graph/001-Graph-Basics/src/SparseGraph.java
@@ -9,51 +9,56 @@ public class SparseGraph implements Graph {
     private Vector<Integer>[] g; // 图的具体数据
 
     // 构造函数
-    public SparseGraph( int n , boolean directed ){
+    public SparseGraph(int n, boolean directed) {
         assert n >= 0;
         this.n = n;
         this.m = 0;    // 初始化没有任何边
         this.directed = directed;
         // g初始化为n个空的vector, 表示每一个g[i]都为空, 即没有任和边
         g = new Vector[n];
-        for(int i = 0 ; i < n ; i ++)
+        for (int i = 0; i < n; i++)
             g[i] = new Vector<Integer>();
     }
 
-    public int V(){ return n;} // 返回节点个数
-    public int E(){ return m;} // 返回边的个数
+    public int V() {
+        return n;
+    } // 返回节点个数
+
+    public int E() {
+        return m;
+    } // 返回边的个数
 
     // 向图中添加一个边
-    public void addEdge( int v, int w ){
+    public void addEdge(int v, int w) {
 
-        assert v >= 0 && v < n ;
-        assert w >= 0 && w < n ;
+        assert v >= 0 && v < n;
+        assert w >= 0 && w < n;
 
         g[v].add(w);
-        if( v != w && !directed )
+        if (v != w && !directed)
             g[w].add(v);
 
-        m ++;
+        m++;
     }
 
     // 验证图中是否有从v到w的边
-    public boolean hasEdge( int v , int w ){
+    public boolean hasEdge(int v, int w) {
 
-        assert v >= 0 && v < n ;
-        assert w >= 0 && w < n ;
+        assert v >= 0 && v < n;
+        assert w >= 0 && w < n;
 
-        for( int i = 0 ; i < g[v].size() ; i ++ )
-            if( g[v].elementAt(i) == w )
+        for (int i = 0; i < g[v].size(); i++)
+            if (g[v].elementAt(i) == w)
                 return true;
         return false;
     }
 
     // 显示图的信息
-    public void show(){
+    public void show() {
 
-        for( int i = 0 ; i < n ; i ++ ){
+        for (int i = 0; i < n; i++) {
             System.out.print("vertex " + i + ":\t");
-            for( int j = 0 ; j < g[i].size() ; j ++ )
+            for (int j = 0; j < g[i].size(); j++)
                 System.out.print(g[i].elementAt(j) + "\t");
             System.out.println();
         }
